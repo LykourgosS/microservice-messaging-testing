7ee3b2c169830fd6a83ad7730489337acbda564a
LykourgosS <lpsarantidis@gmail.com>
2024-09-19 19:26:39 +0300

    feat: configure and start RabbitMq

--- InvoiceMicroservice/Program.cs
+++ InvoiceMicroservice/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using MassTransit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace InvoiceMicroservice
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host("localhost");
+                cfg.ReceiveEndpoint("invoice-service", endpoint =>
+                {
+                    endpoint.UseInMemoryOutbox();
+                    endpoint.Consumer<EventConsumer>(c =>
+                        c.UseMessageRetry(m => m.Interval(5, new TimeSpan(0, 0, 10)))
+                    );
+                });
+            });
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            await busControl.StartAsync(source.Token);
+
+            Console.WriteLine("Invoice Microservice Now Listening");
+
+            try
+            {
+                while (true)
+                {
+                    //sit in while loop listening for messages
+                    await Task.Delay(100);
+                }
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
         }
     }
 }
