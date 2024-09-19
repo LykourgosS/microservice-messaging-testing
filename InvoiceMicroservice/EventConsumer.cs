21f0e8ec2b462b73d1198ae54aef11c3b201a90d
LykourgosS <lpsarantidis@gmail.com>
2024-09-19 19:08:45 +0300

    feat: add EventConsumer (implements IConsumer<IInvoiceToCreate>)

---
+++ InvoiceMicroservice/EventConsumer.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using MessageContracts;
+using System;
+using System.Threading.Tasks;
+
+namespace InvoiceMicroservice
+{
+    public class EventConsumer : IConsumer<IInvoiceToCreate>
+    {
+        public async Task Consume(ConsumeContext<IInvoiceToCreate> context)
+        {
+            var newInvoiceNumber = new Random().Next(10000, 99999);
+            Console.WriteLine($"Creating invoice {newInvoiceNumber} for customer:{context.Message.CustomerNumber}");
+            context.Message.InvoiceItems.ForEach(item => { Console.WriteLine(item.ToString()); });
+
+            await Publish(context, newInvoiceNumber);
+        }
+
+        private async Task Publish(ConsumeContext<IInvoiceToCreate> context, int newInvoiceNumber)
+        {
+            await context.Publish<IInvoiceCreated>(new
+            {
+                InvoiceNumber = newInvoiceNumber,
+                InvoiceData = new
+                {
+                    context.Message.CustomerNumber,
+                    context.Message.InvoiceItems
+                },
+            });
+        }
+    }
+}
