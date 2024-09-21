cc96ac0aeb986a29471dd6e0b8439633bc8ec46d
LykourgosS <lpsarantidis@gmail.com>
2024-09-21 14:59:35 +0300

    feat: start service for creating invoices

---
+++ TestClient/InvoiceCreatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using MessageContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    public class InvoiceCreatedConsumer : IConsumer<IInvoiceCreated>
+    {
+        public async Task Consume(ConsumeContext<IInvoiceCreated> context)
+        {
+            await Task.Run(() =>
+                Console.WriteLine($"Invoice with number:{context.Message.InvoiceNumber} was created.")
+            );
+        }
+    }
+}
--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MassTransit;
+using MessageContracts;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestClient
 {
@@ -6,7 +11,65 @@ namespace TestClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StartService();
+        }
+
+        private static async void StartService()
+        {
+            Console.WriteLine("Waiting while consumers initialize.");
+            await Task.Delay(3000); //because the consumers need to start first
+
+            var busControl = BusControlCreator.CreateUsingRabbitMq<InvoiceCreatedConsumer>("invoice-service-created");
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+            var keyCount = 0;
+
+            try
+            {
+                Console.WriteLine("Enter any key to send an invoice request or Q to quit.");
+
+                while (Console.ReadKey(true).Key != ConsoleKey.Q)
+                {
+                    keyCount++;
+                    await SendRequestForInvoiceCreation(busControl);
+                    Console.WriteLine($"Enter any key to send an invoice request or Q to quit. {keyCount}");
+                }
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+
+        private static async Task SendRequestForInvoiceCreation(IPublishEndpoint publishEndpoint)
+        {
+            var random = new Random();
+            await publishEndpoint.Publish<IInvoiceToCreate>(new {
+                CustomerNumber = random.Next(1000, 9999),
+                InvoiceItems = new List<InvoiceItems>()
+                {
+                    new() {
+                        Description="Tables",
+                        Price=Math.Round(random.NextDouble()*100,2),
+                        ActualMileage = 40,
+                        BaseRate=12.50,
+                        IsHazardousMaterial=false,
+                        IsOversized=true,
+                        IsRefrigerated=false
+                    },
+                    new() {
+                        Description="Chairs",
+                        Price=Math.Round(random.NextDouble()*100,2),
+                        ActualMileage = 40,
+                        BaseRate=12.50,
+                        IsHazardousMaterial=false,
+                        IsOversized=false,
+                        IsRefrigerated=false
+                    }
+                }
+            });
         }
     }
 }
