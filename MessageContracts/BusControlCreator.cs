058e41eedf21853597274cafce8ac00c88b69d86
LykourgosS <lpsarantidis@gmail.com>
2024-09-21 12:53:50 +0300

    feat: create BusControlCreator with CreateUsingRabbitMq method
    install MassTransit.RabbitMQ package

---
+++ MessageContracts/BusControlCreator.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using System;
+
+namespace MessageContracts
+{
+    public static class BusControlCreator
+    {
+        public static IBusControl CreateUsingRabbitMq<TConsumer>(string serviceName)
+            where TConsumer : class, IConsumer, new()
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host("localhost");
+                cfg.ReceiveEndpoint(serviceName, endpoint =>
+                {
+                    endpoint.UseInMemoryOutbox();
+                    endpoint.Consumer<TConsumer>(c =>
+                        c.UseMessageRetry(m => m.Interval(5, new TimeSpan(0, 0, 10)))
+                    );
+                });
+            });
+        }
+    }
+}
