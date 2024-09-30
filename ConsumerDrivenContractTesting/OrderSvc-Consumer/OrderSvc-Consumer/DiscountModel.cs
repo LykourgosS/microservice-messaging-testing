6b1cce007585fb72aaa84de0b390a29d6237e84a
LykourgosS <lpsarantidis@gmail.com>
2024-09-30 15:22:41 +0300

    feat: add DiscountModel class

---
+++ ConsumerDrivenContractTesting/OrderSvc-Consumer/OrderSvc-Consumer/DiscountModel.cs
@@ -0,0 +1,9 @@
+namespace OrderSvc_Consumer
+{
+    public class DiscountModel
+    {
+        public double CustomerRating { get; set; }
+
+        public double AmountToDiscount { get; set; }
+    }
+}
